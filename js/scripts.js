466451699707bf23cc8489d94ec662bde2321151
paintrain86 <webmaster.karpov@yandex.ru>
2016-06-28 02:11:25 +0300

    after changes-rebase client fixes - popup pages

--- js/scripts.js
+++ js/scripts.js
@@ -62,44 +62,6 @@ $(function() {
     }
   })
 
-  /*===================== POPUP =====================*/
-  var gTown = getCookie('guesstown');
-  if (gTown == undefined) {
-    setTimeout(function() {
-      $('#guess-town-modal').modal();
-    }, 1000);
-  } else {
-    $('#modal-town #town-list a').each(function() {
-      if ($(this).text() == gTown) {
-        $(this).parent().siblings().removeClass('active');
-        $(this).parent().addClass('active');
-      }
-    });
-  }
-
-  $('#guess-town-modal .btn-custom').click(function(e) {
-    if ($(e.target).attr('data-guess') == 'yes') {
-      setCookie('guesstown', $('#guess-town-modal #guess-town').text());
-      $('#guess-town-modal').modal('hide');
-    } else {
-      $('#guess-town-modal').modal('hide');
-      setTimeout(function() {
-        $('#modal-town').modal();
-      }, 500);
-    }
-  });
-  $('#modal-town #town-list a').click(function(e) {
-    e.preventDefault();
-    setCookie('guesstown', $(e.target).text());
-    $('#modal-town').modal('hide');
-    $('#modal-town #town-list a').each(function() {
-      if ($(this).text() == $(e.target).text()) {
-        $(this).parent().siblings().removeClass('active');
-        $(this).parent().addClass('active');
-      }
-    });
-  });
-
   /*===================== FILTER FUNCTIONS =====================*/
   $('.filter__dropdown-toggle').click(function(e){
     e.preventDefault();
@@ -118,45 +80,3 @@ $(function() {
     });
     $('input[name="price-from"]').val($('#price-range').slider('values', 0 ))
 });
-/*===================== GLOBAL FUNCTIONS =====================*/
-function getCookie(name) {
-  var matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-function setCookie(name, value, options) {
-  options = options || {};
-
-  var expires = options.expires;
-
-  if (typeof expires == "number" && expires) {
-    var d = new Date();
-    d.setTime(d.getTime() + expires * 1000 * 60 * 60 * 24); //cookie for one day
-    expires = options.expires = d;
-  }
-  if (expires && expires.toUTCString) {
-    options.expires = expires.toUTCString();
-  }
-
-  value = encodeURIComponent(value);
-
-  var updatedCookie = name + "=" + value;
-
-  for (var propName in options) {
-    updatedCookie += "; " + propName;
-    var propValue = options[propName];
-    if (propValue !== true) {
-      updatedCookie += "=" + propValue;
-    }
-  }
-
-  document.cookie = updatedCookie;
-}
-
-function deleteCookie(name) {
-  setCookie(name, "", {
-    expires: -1
-  })
-}
