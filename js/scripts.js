c32d0a7b0df1b3fd1485c5bb8f85e76233726661
paintrain86 <webmaster.karpov@yandex.ru>
2016-07-05 09:12:04 +0300

    datepicker

--- js/scripts.js
+++ js/scripts.js
@@ -12,15 +12,15 @@ $(function() {
     $('#guess-town-modal').modal();
   }, 1000);
   $('#guess-town-modal .btn-custom').click(function(e) {
-     if ($(e.target).attr('data-guess') == 'yes') {
-       $('#guess-town-modal').modal('hide');
-     } else {
-       $('#guess-town-modal').modal('hide');
-       setTimeout(function() {
-         $('#modal-town').modal();
-       }, 500);
-     }
-   });
+    if ($(e.target).attr('data-guess') == 'yes') {
+      $('#guess-town-modal').modal('hide');
+    } else {
+      $('#guess-town-modal').modal('hide');
+      setTimeout(function() {
+        $('#modal-town').modal();
+      }, 500);
+    }
+  });
 
   /*===================== CATLIST DROPDOWN ON HOVER =====================*/
   $('.catlist__title.dropdown').hover(function() {
@@ -85,75 +85,99 @@ $(function() {
 
   /*===================== INPUT NUMBERS =====================*/
   var pressTimer,
-      controlASC = $('<span/>',{
+    controlASC = $('<span/>', {
       'class': 'number__control number__control--asc'
     }),
-    controlDESC = $('<span/>',{
+    controlDESC = $('<span/>', {
       'class': 'number__control number__control--desc'
     });
 
-    $('.number-custom input[type="number"]').each(function(){
-      controlDESC.clone().appendTo($(this).parent());
-      controlASC.clone().appendTo($(this).parent());
-    });
+  $('.number-custom input[type="number"]').each(function() {
+    controlDESC.clone().appendTo($(this).parent());
+    controlASC.clone().appendTo($(this).parent());
+  });
 
-    $('.number__control').mouseup(function(){
-        clearTimeout(pressTimer);
-      })
-    .mousedown(function(e){
+  $('.number__control').mouseup(function() {
+      clearTimeout(pressTimer);
+    })
+    .mousedown(function(e) {
       pressTimer = window.setTimeout(function() {
         numberInputChange(e);
-      },50);
+      }, 50);
 
       return false;
     });
 
-    function numberInputChange(e) {
-      var parent = $(e.target).parent(),
-            input = $('input[type="number"]', parent),
-            val = parseInt(input.val()),
-            min = parseInt(input.attr('min')) - 1,
-            max = parseInt(input.attr('max')) + 1;
+  function numberInputChange(e) {
+    var parent = $(e.target).parent(),
+      input = $('input[type="number"]', parent),
+      val = parseInt(input.val()),
+      min = parseInt(input.attr('min')) - 1,
+      max = parseInt(input.attr('max')) + 1;
 
-        if ($(e.target).hasClass('number__control--asc')) {
-          if ((val + 1) < max) {
-            input.val(val + 1);
-          }
-        } else if ($(e.target).hasClass('number__control--desc')) {
-          if ((val - 1) > min) {
-            input.val(val - 1);
-          }
-        } else if ($(e.target).is(input)){
-          if (input.val() > max) {
-            input.val(max - 1);
-          } else if (input.val() < min || input.val() == NaN) {
-            input.val(min + 1);
-          }
-        }
+    if ($(e.target).hasClass('number__control--asc')) {
+      if ((val + 1) < max) {
+        input.val(val + 1);
+      }
+    } else if ($(e.target).hasClass('number__control--desc')) {
+      if ((val - 1) > min) {
+        input.val(val - 1);
+      }
+    } else if ($(e.target).is(input)) {
+      if (input.val() > max) {
+        input.val(max - 1);
+      } else if (input.val() < min || input.val() == NaN) {
+        input.val(min + 1);
+      }
     }
+  }
   /*===================== FILTER FUNCTIONS =====================*/
-  $('.filter__dropdown-toggle').click(function(e){
+  $('.filter__dropdown-toggle').click(function(e) {
     e.preventDefault();
     $(this).parent().toggleClass('active');
   });
 
   $('#price-range').slider({
-      range: true,
-      min: 0,
-      max: 2999,
-      values: [2499, 2874],
-      slide: function( event, ui ) {
-        $('input[name="price-from"]').val(ui.values[0]);
-        $('input[name="price-to"]').val(ui.values[1]);
-      }
-    });
-    $('input[name="price-from"]').val($('#price-range').slider('values', 0 ))
+    range: true,
+    min: 0,
+    max: 2999,
+    values: [2499, 2874],
+    slide: function(event, ui) {
+      $('input[name="price-from"]').val(ui.values[0]);
+      $('input[name="price-to"]').val(ui.values[1]);
+    }
+  });
+  $('input[name="price-from"]').val($('#price-range').slider('values', 0))
 
-    /*===================== ORDER =====================*/
-    $('#order-map-btn').click(function(e){
-      e.preventDefault();
-      var newText = ($(this).text() == 'Показать карту') ? 'Скрыть карту' : 'Показать карту';
-      $(this).text(newText);
-      $('#order-map-wrap').slideToggle();
-    });
+  /*===================== CALENDAR =====================*/
+  $.datepicker.regional['ru'] = {
+    closeText: 'Закрыть',
+    prevText: 'Пред',
+    nextText: 'След',
+    currentText: 'Сегодня',
+    monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+    ],
+    monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн',
+      'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'
+    ],
+    dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+    dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+    dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    minDate: 0,
+    isRTL: false
+  };
+  $.datepicker.setDefaults($.datepicker.regional['ru']);
+
+  $('.datepicker').datepicker();
+
+  /*===================== ORDER =====================*/
+  $('#order-map-btn').click(function(e) {
+    e.preventDefault();
+    var newText = ($(this).text() == 'Показать карту') ? 'Скрыть карту' : 'Показать карту';
+    $(this).text(newText);
+    $('#order-map-wrap').slideToggle();
+  });
 });
